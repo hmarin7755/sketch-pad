b2df7fb0b6e386f5defa6c3196791cd7f5506edf
Humberto Marin <hmarin2698@gmail.com>
2022-09-25 01:05:22 -0400

    Create button for custom size of board

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-    createGrid(16);
+  createGrid(16);
+  let btnPopup = document.querySelector("#popup");
+  btnPopup.addEventListener("click", function () {
+    let size = getSize();
+    createGrid(size);
+  
+  });
 });
 
-function createGrid(size){
-    let canvas = document.querySelector(".canvas");
-    canvas.style.gridTemplateColumns = `repeat(${size},1fr)`;
-    canvas.style.gridTemplateRows = `repeat(${size},1fr)`;
+function createGrid(size) {
+  let canvas = document.querySelector(".canvas");
+  canvas.style.gridTemplateColumns = `repeat(${size},1fr)`;
+  canvas.style.gridTemplateRows = `repeat(${size},1fr)`;
 
-    let numDivs = size*size;
-    for (let i = 0; i < numDivs; i++){
-        let div = document.createElement("div");
-        canvas.insertAdjacentElement("beforeend", div);
-        
-    }
+  let numDivs = size * size;
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement("div");
+    canvas.insertAdjacentElement("beforeend", div);
+  }
+}
+
+function getSize() {
+  let input = prompt("Enter Size (1 - 100)");
+  let message = document.querySelector("#message");
+  if (input === "") {
+    message.innerHTML = "Please provide a number";
+  } else if (input <= 0 || input > 100) {
+    message.innerHTML = "Please Provide a number between 1 and 100";
+  } else {
+    message.innerHTML = `${input}x${input}`;
+    return input;
+  }
 }
